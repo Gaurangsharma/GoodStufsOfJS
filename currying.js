8314017b91d971321ef23bf029675eb180da9ada
Gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2020-08-06 15:23:03 +0530

    function curring

---
+++ currying.js
@@ -0,0 +1,22 @@
+let multiply=(x,y)=>{
+    console.log(x*y);
+}
+
+//bind method
+
+const multiplybytwo= multiply.bind(this,2);
+multiplybytwo(5);
+
+
+const multiplybyThree= multiply.bind(this,7);
+multiplybyThree(5);
+
+
+//clouser method
+let multiplyy=function(x){
+    return function(y){
+        console.log(x*y);
+    }
+}
+let multiByTwo=multiplyy(2);
+multiByTwo(9);
